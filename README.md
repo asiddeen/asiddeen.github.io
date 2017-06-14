c4b5f4215983d300726e5d00d384183d45d9804e
Asiddeen <asiddeen@gmail.com>
2017-06-14 14:21:55 +0000

    Ini

--- README.md
+++
@@ -1,1 +0,0 @@
-# asiddeen.github.io
---
+++ assets/assets/js/contact.js
@@ -0,0 +1,21 @@
+$(function () {
+  "use strict";
+
+  $('#contact-form').validator();
+  $("#contact-form :input").change(function() {
+
+  });
+
+
+  $('#contact-form').on('submit', function (e) {
+    if (!e.isDefaultPrevented()) {
+      e.preventDefault();
+      var name = $("#form_name").val()+' '+$("#form_lastname").val();
+      var email = $("#form_email").val();
+      var mobile = $("#form_phone").val();
+      var msg = $("#form_message").val();
+      var url = 'mailto:'+email+'?body='+name+'\n'+mobile+'\n\n***\n'+msg;
+      window.open(url,'_blank');
+    }
+  })
+});
